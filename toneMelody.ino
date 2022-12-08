a7e1fc3fe7e2ae910fc2a19a7f25a0ffa847a909
Forrest Erickson <forresterickson@comcast.net>
2022-12-07 22:57:32 -0500

    Make function to playMelody. Experiment with several octaves of sound.

--- toneMelody.ino
+++ toneMelody.ino
@@ -18,26 +18,25 @@
 #include "pitches.h"
 
 #define PROG_NAME "******toneMelody******"     //Descriptive name of this software, 20 characters.
+#define VERSION "Version: 0.1"     //Version of this firmware.
 #define TONE_PIN 8
 #define BAUDRATE 115200
 
-// notes in the melody:
-int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
 
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
-};
+void playMelody() {
+  // notes in the melody:
+  int melody[] = {
+//    NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+//    NOTE_C5, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, 0, NOTE_B4, NOTE_C5
+//    NOTE_C6, NOTE_G5, NOTE_G5, NOTE_A5, NOTE_G5, 0, NOTE_B5, NOTE_C6
+    NOTE_C7, NOTE_G6, NOTE_G6, NOTE_A6, NOTE_G6, 0, NOTE_B6, NOTE_C7
+  };
 
-void setup() {
-  //Serial setup
-  delay(100);
-  Serial.begin(BAUDRATE);
-  delay(100);                         //Wait before sending the first data to terminal
-  Serial.println(PROG_NAME);
-  
+  // note durations: 4 = quarter note, 8 = eighth note, etc.:
+  int noteDurations[] = {
+    4, 8, 8, 4, 4, 4, 4, 4
+  };
+
   // iterate over the notes of the melody:
   //Play a melody
   for (int thisNote = 0; thisNote < 8; thisNote++) {
@@ -55,6 +54,16 @@ void setup() {
     noTone(TONE_PIN);
   }// end play a melody
 }
+void setup() {
+  //Serial setup
+  delay(100);
+  Serial.begin(BAUDRATE);
+  delay(100);                         //Wait before sending the first data to terminal
+  Serial.println(F(PROG_NAME));
+  Serial.println(F(VERSION));
+
+  playMelody();
+}//end setup
 
 void loop() {
   // no need to repeat the melody.
